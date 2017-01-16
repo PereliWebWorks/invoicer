dd409009a1a8f1de3130799288084ad9d378b5e9
Andrew Pereli <drewpereli@gmail.com>
2017-01-15 20:56:38 -0600

    generalizing CRUD buttons

--- public_html/index.php
+++ public_html/index.php
@@ -7,12 +7,18 @@
 	</div>
 	<div class="row client-list">
 		<?php foreach (getCurrentUser()->getClients() as $client) : ?>
-			<div class="col-xs-12">
+			<div class="col-xs-8">
 				<a href="client.php?c=<?= $client->id; ?>">
 					<h4><?= $client->name; ?></h4>
 				</a>
 				<div><i><?= $client->email; ?></i></div>
 			</div>
+			<input type="button" 
+				class="col-xs-2 btn btn-danger CRUD-btn"
+				data-model="Client"
+				data-action="destroy"
+				data-id="<?= $client->id; ?>"
+				value="Delete">
 		<?php endforeach ?>
 	</div>
 	
